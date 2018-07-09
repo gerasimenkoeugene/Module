02550705afc417dc76ef1bcb2ff819938c79d795
gerasimenkoeugene <gerasymenkoeugene@gmail.com>
2018-07-09 16:17:20 +0300

    FLEXAPP-2742 added _1

---
+++ testScript/commits.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+REPO=$1
+NOW=$(date +'%d-%m-%Y-%r')
+BRANCH_INDEX=$master
+
+git checkout master
+git pull
+
+for i in {1..45}
+do
+    echo "text2" > ${BRANCH_INDEX}_${i}.txt
+    git add .
+    git commit -m "FLEXAPP-2742 added "${BRANCH_INDEX}_${i}" "   
+	git push 	
+done
+
